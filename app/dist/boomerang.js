b025d6046252c46a4e948a77aa57dd0fa3081924
Michael Prentice <splaktar@gmail.com>
2016-07-23 11:40:24 -0400

    chore(build): build dist for 2.2.0

--- app/dist/boomerang.js
+++ app/dist/boomerang.js
@@ -303,6 +303,15 @@ angular.module('gdgXBoomerang')
     getPastEventsPage(1);
 });
 
+'use strict';
+
+angular.module('gdgXBoomerang').directive('boomerangFooter', function() {
+    return {
+        templateUrl: '/app/footer/footer.html',
+        restrict: 'E'
+    };
+});
+
 // Google+ hashtag linky from http://plnkr.co/edit/IEpLfZ8gO2B9mJcTKuWY?p=preview
 angular.module('gdgXBoomerang')
 .filter('hashLinky', function() {
@@ -501,33 +510,6 @@ angular.module('gdgXBoomerang')
         });
 });
 
-'use strict';
-
-angular.module('gdgXBoomerang')
-.directive('gplusPerson', function ($http, $filter, Config) {
-    return {
-        restrict: 'EA',
-        templateUrl: 'app/organizers/components/gplus_person.html',
-        scope: {
-            gplusId: '='
-        },
-        link: function (scope) {
-            scope.$watch('gplusId', function (oldVal, newVal) {
-                if (newVal) {
-                    $http.jsonp('https://www.googleapis.com/plus/v1/people/' + newVal +
-                        '?callback=JSON_CALLBACK&fields=aboutMe%2CdisplayName%2Cimage&key=' + Config.googleApi)
-                        .success(function (data) {
-                            if (data && data.image && data.image.url) {
-                                data.image.url = data.image.url.replace('sz=50', 'sz=170');
-                            }
-                            scope.person = data;
-                        });
-                }
-            });
-        }
-    };
-});
-
 angular.module('gdgXBoomerang')
 .directive('gplusAlbum', function () {
     return {
@@ -609,5 +591,32 @@ angular.module('gdgXBoomerang')
 .directive('newsItemFooter', function () {
     return {
         templateUrl: 'app/news/components/newsItemFooter.html'
+    };
+});
+
+'use strict';
+
+angular.module('gdgXBoomerang')
+.directive('gplusPerson', function ($http, $filter, Config) {
+    return {
+        restrict: 'EA',
+        templateUrl: 'app/organizers/components/gplus_person.html',
+        scope: {
+            gplusId: '='
+        },
+        link: function (scope) {
+            scope.$watch('gplusId', function (oldVal, newVal) {
+                if (newVal) {
+                    $http.jsonp('https://www.googleapis.com/plus/v1/people/' + newVal +
+                        '?callback=JSON_CALLBACK&fields=aboutMe%2CdisplayName%2Cimage&key=' + Config.googleApi)
+                        .success(function (data) {
+                            if (data && data.image && data.image.url) {
+                                data.image.url = data.image.url.replace('sz=50', 'sz=170');
+                            }
+                            scope.person = data;
+                        });
+                }
+            });
+        }
     };
 });
